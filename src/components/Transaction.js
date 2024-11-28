d5b2e9b1c8221f01b66abb70be9c3536aa0cc32e
mohamedshaban <sarahmama@moe.edu.kw>
2024-11-28 12:08:29 +0300

    search activated

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -3,41 +3,49 @@ import React, { useState } from "react";
 import { transaction } from "../api/auth";
 
 const Transaction = () => {
-  const transType = ["all", "deposit", "withdraw", "transfer"]
+  const transType = ["all", "deposit", "withdraw", "transfer"];
+
   const { data, isFetching, isSuccess, error } = useQuery({
     queryKey: ["transaction"],
     queryFn: () => transaction(),
   });
 
-
-
-  // Filter state for transactions
   const [filter, setFilter] = useState("all");
-  // Apply filtering
-  const filteredTransactions =
-    filter === "all"
-      ? data
-      : data?.filter((transaction) => transaction.type === filter)
-      ;
-const handleOnChange =()=>{
-  
-}
+  const [searchTerm, setSearchTerm] = useState("");
+  const [fromDate, setFromDate] = useState("");
+  const [toDate, setToDate] = useState("");
+
+  const filteredTransactions = data?.filter((transaction) => {
+    if (filter !== "all" && transaction.type !== filter) return false;
+
+    if (
+      searchTerm &&
+      !transaction.type.toLowerCase().includes(searchTerm.toLowerCase()) &&
+      !transaction.amount.toString().includes(searchTerm)
+    )
+      return false;
+
+    const transactionDate = new Date(transaction.updatedAt);
+    if (fromDate && transactionDate < new Date(fromDate)) return false;
+    if (toDate && transactionDate > new Date(toDate)) return false;
+
+    return true;
+  });
+
   return (
     <div className="App">
-     
-      <div className="w-50 mb-4">
+      <div className="w-50 mb-4 mt-4">
         <input
           type="text"
           name="search"
-          placeholder="Search"
-          className="border border-black mx-3 px-3 w-75 "
-     
+          placeholder="Search by type or amount"
+          className="border border-black mx-3 px-3 w-75"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <button type="submit" className="btn butt">
-          Search
-        </button>
       </div>
 
+      {/* Filter by Type */}
       <div className="trans-container px-5 py-2">
         <div className="d-flex justify-content-center mb-3">
           <label className="text-color">Filter: </label>
@@ -63,37 +71,46 @@ const handleOnChange =()=>{
         <div className="d-flex justify-content-center">
           <div className="mx-5">
             <label className="text-color">From:</label>
-            <input type="date" name="fromDate" className="m-2 px-2" />
+            <input
+              type="date"
+              name="fromDate"
+              className="m-2 px-2"
+              value={fromDate}
+              onChange={(e) => setFromDate(e.target.value)}
+            />
           </div>
           <div className="mx-5">
             <label className="text-color">To:</label>
-            <input type="date" name="toDate" className="m-2 px-2" />
+            <input
+              type="date"
+              name="toDate"
+              className="m-2 px-2"
+              value={toDate}
+              onChange={(e) => setToDate(e.target.value)}
+            />
           </div>
         </div>
       </div>
 
       {/* Transactions List */}
       <div className="login-container px-5 py-3 mt-4 m-2">
-     <div className="container"> 
-      <div className="row">
-      <h4 className="col-4">Type</h4>
-      <h4 className="col-4">Amount</h4>
-      <h4 className="col-4">Date</h4> 
-      </div>
-     </div>
-   
-    {filteredTransactions?.map((transaction)=>(<div className="container m-2" key={transaction.id}> 
-      <div className="row">
-      <h4 className="col-4">{transaction.type}</h4>
-      <h4 className="col-4">{transaction.amount}</h4>
-      <h4 className="col-4">{transaction.updatedAt.slice(0,10)}</h4> 
-      </div>
-     </div>))} 
-             
-       
-   
-        
-        
+        <div className="container">
+          <div className="row">
+            <h4 className="col-4">Type</h4>
+            <h4 className="col-4">Amount</h4>
+            <h4 className="col-4">Date</h4>
+          </div>
+        </div>
+
+        {filteredTransactions?.map((transaction) => (
+          <div className="container m-2" key={transaction.id}>
+            <div className="row">
+              <h4 className="col-4">{transaction.type}</h4>
+              <h4 className="col-4">{transaction.amount}</h4>
+              <h4 className="col-4">{transaction.updatedAt.slice(0, 10)}</h4>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
